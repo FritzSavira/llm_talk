64e58f44290bb20ba1040f6f7300fda27a57a54e
Friedemann <friedemann.decker@online.de>
2025-01-18 19:55:12 +0100

    main.py: Leerzeilen eingefügt

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,11 +2,13 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+
 @app.route('/hallo', methods=['GET'])
 def hallo():
     message = "Hallo Welt"
     print(message)  # Gibt "Hallo Welt" in der Server-Konsole aus
     return jsonify({'message': message})
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
