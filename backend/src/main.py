89dd472a581b7fd1dd7e1247a13e55eaea741131
FritzSavira <Friedemann.Decker@online.de>
2025-01-18 15:26:04 +0100

    main.py, test_main.py: aktualisiert

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 def add(a, b):
     return a + b
 
+
 print("test")
 print("Neuer Test 2")
--- backend/src/tests/test_main.py
+++ backend/src/tests/test_main.py
@@ -1,5 +1,6 @@
 # test_main.py
 from backend.src.main import add
 
+
 def test_add():
     assert add(2, 3) == 5
