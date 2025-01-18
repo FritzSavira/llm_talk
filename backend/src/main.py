c8978234567b2b3d011340b7db9c73ad08efbb01
Friedemann <friedemann.decker@online.de>
2025-01-18 19:51:34 +0100

    Initiales Test-Skript, um frontend und backend zu verknüpfen.

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,12 @@
-def add(a, b):
-    return a + b
+from flask import Flask, jsonify
 
+app = Flask(__name__)
 
-print("test")
-print("Neuer Test 2")
-print("Hallo Welt")
+@app.route('/hallo', methods=['GET'])
+def hallo():
+    message = "Hallo Welt"
+    print(message)  # Gibt "Hallo Welt" in der Server-Konsole aus
+    return jsonify({'message': message})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
